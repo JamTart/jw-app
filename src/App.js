42ba45206112a2c47c5550548bb52b64fd6938dd
JamTart <jamalwinchester@gmail.com>
2021-04-22 17:20:45 -0400

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+      cats:[]
+    };
+  }
+   componentDidMount(){
+     fetch('https://thatcopy.pw/catapi/rest/')
+     .then(response=>response.json())
+     .then(pics => this.setState({cats:pics}));
+     //.then(pics => console.log(pics));
+   }
+   render(){
+    return(
+    <div className="App" div className="App">{
+     
+      //this.state.cats.map(cat => <h1 key={cat.id}>{cat.url}</h1>)
+      //<div>{this.state.cats.url}</div>
+      <div style={{backgroundColor:"blanchedalmond"}}>
+        <div>
+          <h1 style={{color:"teal", font:"Fira Sans"}}> Cat Heaven</h1>
+        </div>
+        <div>
+        <img width="500" height="500" src={this.state.cats.url}/>
+        </div>
+      </div>
+    }
     </div>
-  );
-}
+    );
+  }
+  }
+ 
 
 export default App;
